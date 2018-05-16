9507b0138260bac87d708c1cf99ebf48a3292f06
Panagiotis Margaris <takismarg@gmail.com>
2018-05-16 23:18:44 +0100

    Fix Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -3,10 +3,9 @@ push!(LOAD_PATH,"../src/")
 using Documenter, EconTools
 
 makedocs(modules = [EconTools])
-#=
+
 deploydocs(
     deps   = Deps.pip("mkdocs", "python-markdown-math"),
     repo = "github.com/pmarg/EconTools.jl.git",
     julia  = "0.6"
 )
-=#
