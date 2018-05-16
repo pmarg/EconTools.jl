6cbbb8719fe3022db6636e7e0ab8447632e2e2ba
Panagiotis Margaris <takismarg@gmail.com>
2018-05-16 23:57:24 +0100

    clean DataSetTools

--- src/DataSetTools.jl
+++ src/DataSetTools.jl
@@ -4,8 +4,12 @@ import JuliaDB: DNextTable
 """
     descriptive_stats(table::DNextTable,var::String,by::Tuple;weight="None")
 Returns a new table containing  the mean, standard deviation, 1st, 2nd and 3rd quantiles of
-variable `var` by a selection of tuple of columns `by`. Weights are optional and if specified,
+variable `var` by a selection of tuple of columns `bycolumns`. Weights are optional and if specified,
 the function returns the weighted descriptive statistics.
+
+For example `descriptive_stats(data,"income",(:age,:gender),weight="sampling_weights")` returns
+the descriptive statistics of income from table `data` by the columns name `age` and `gender`, weighted by the
+sampling weights `sampling_weights`
 """
 function descriptive_stats(table::DNextTable,var::String,by::Tuple;weight="None")
     if weight=="None"
@@ -16,7 +20,7 @@ function descriptive_stats(table::DNextTable,var::String,by::Tuple;weight="None"
         median1 = median,
         q75=z->quantile(z,0.25)
         ),
-        table,by,select=Symbol(var))
+        table,bycolumns,select=Symbol(var))
     else
         y= groupby(@NT(
         avg=z->mean(column(z,Symbol(var)),weights(column(z,Symbol(weight)))),
@@ -25,14 +29,16 @@ function descriptive_stats(table::DNextTable,var::String,by::Tuple;weight="None"
         median1=z->median(column(z,Symbol(var)),weights(column(z,Symbol(weight)))),
         q75=z->quantile(column(z,Symbol(var)),0.75)
         ),
-        table,by,select=(Symbol(var),Symbol(weight)))
+        table,bycolumns,select=(Symbol(var),Symbol(weight)))
     end
     return y
 end
 
 """
     save_csv(path::String,table::DNextTable)
-    Saves table as CSV at the specified path
+Saves table as CSV at the specified path.
+For example save_csv("Data\\data.csv",data) saves the table `data` in the
+subfolder `Data` under the name `data.csv`
 """
 function save_csv(path::String,table::DNextTable)
     open(path,"w") do fid
