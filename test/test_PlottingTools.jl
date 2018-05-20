ac3390e9a5ac1c9eb92dd5b39c85b11a58f777bd
Panagiotis Margaris <takismarg@gmail.com>
2018-05-20 21:38:10 +0100

    fix test

--- test/test_PlottingTools.jl
+++ test/test_PlottingTools.jl
@@ -6,6 +6,3 @@ y2 = linspace(10,0,10)
 
 @test scatterplot(x1,y1) isa(Plots.Plot)
 @test scatterplot(x1,y2) isa(Plots.Plot)
-
-@test pgfplots_scatter(x1,y1) isa(PGFPlotsX.TikzPicture)
-@test pgfplots_scatter(x1,y2) isa(PGFPlotsX.TikzPicture)
