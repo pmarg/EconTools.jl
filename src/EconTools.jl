79ebc2bd0f3dd0fedbc8d7e72b6bbc67a6d6f947
Panagiotis Margaris <takismarg@gmail.com>
2019-04-23 10:22:41 +0200

    rm JuliaDB

--- src/EconTools.jl
+++ src/EconTools.jl
@@ -1,15 +1,16 @@
 module EconTools
 
-using Statistics, Distributions, StatsFuns, Interpolations, JuliaDB, Statistics, StatsBase
+using Statistics, Distributions, StatsFuns, Interpolations,  Statistics, StatsBase
 using PGFPlotsX
+#using JuliaDB
 import DataFrames:DataFrame
 
 
 
-include("QuantTools.jl")
+#include("QuantTools.jl")
 include("Utilities.jl")
 include("Grids.jl")
 include("DataSetTools.jl")
 
-export expspace, tauchen, indices, simulate_markov_shocks, stata_coordinates, interpolate_params,summarise
+export expspace, tauchen, indices, simulate_markov_shocks, stata_coordinates, interpolate_params,#summarise
 end # module
