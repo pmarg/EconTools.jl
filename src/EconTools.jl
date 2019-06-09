ec46a08b82cc0842825b0cf639a78465b15c748e
Takis <takismarg@gmail.com>
2019-06-09 18:28:08 +0200

    move to RTools.jl

--- src/EconTools.jl
+++ src/EconTools.jl
@@ -10,10 +10,8 @@ using PGFPlotsX, FreqTables, DataFrames
 include("QuantTools.jl")
 include("Utilities.jl")
 include("Grids.jl")
-include("R.jl")
 include("DataSetTools.jl")
 
 export expspace, tauchen, indices, simulate_markov_shocks, stata_coordinates, interpolate_params
-export load_rds, save_rds, initialize_survey, svyby
 export keep!, tabulate, reshape_results!
 end # module
