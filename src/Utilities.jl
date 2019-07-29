bf43ae0bcf8d540cc83f7f394b9e4616d2143d65
pmarg <takismarg@gmail.com>
2019-07-29 17:26:36 +0300

    add method for float in prettytime

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -47,3 +47,29 @@ function prettytime(t::Int)
     end
     return lpad(str, 6, " ")
 end
+function prettytime(t::AbstractFloat)
+    if t < 1e3
+        value, units = t, "ns"
+    elseif t < 1e6
+        value, units = t / 1e3, "μs"
+    elseif t < 1e9
+        value, units = t / 1e6, "ms"
+    else
+        if t / 1e9 < 60
+          value, units = t / 1e9, "s"
+        elseif t / 1e9 < 3600
+          value, units = t / (1e9*60), "m"
+        else
+          value, units = t / (1e9*60*60), "h"
+        end
+    end
+
+    if round(value) >= 100
+        str = string(@sprintf("%.0f", value), units)
+    elseif round(value * 10) >= 100
+        str = string(@sprintf("%.1f", value), units)
+    else
+        str = string(@sprintf("%.2f", value), units)
+    end
+    return lpad(str, 6, " ")
+end
