60df2343f36346ff77d9b1818aade5dfe89d6c04
pmarg <takismarg@gmail.com>
2019-07-29 16:23:45 +0300

    add TimerOutputs

--- src/EconTools.jl
+++ src/EconTools.jl
@@ -2,6 +2,7 @@ module EconTools
 
 using Statistics, Distributions, StatsFuns, Interpolations,  Statistics, StatsBase
 using PGFPlotsX, FreqTables, DataFrames
+import TimerOutputs:prettytime
 #using JuliaDB
 
 
@@ -14,4 +15,5 @@ include("DataSetTools.jl")
 
 export expspace, tauchen, indices, simulate_markov_shocks, stata_coordinates, interpolate_params
 export keep!, tabulate, reshape_results!, print_struct, print_parameters, percentiles!
+export to, prettytime
 end # module
--- src/Utilities.jl
+++ src/Utilities.jl
@@ -19,3 +19,31 @@ function indices(n_a, n_h, n_zl, n_zh, n_zm,ind)
 
     return i_a, i_h, i_zl, i_zh, i_zm
 end
+
+const to = TimerOutput()
+function prettytime(t)
+    if t < 1e3
+        value, units = t, "ns"
+    elseif t < 1e6
+        value, units = t / 1e3, "μs"
+    elseif t < 1e9
+        value, units = t / 1e6, "ms"
+    else
+        if t / 1e9 < 60
+          value, units = t / 1e9, "s"
+        elseif t / 1e9 < 3600
+          value, units = t / (1e9*60), "min"
+        else
+          value, units = t / (1e9*60*60), "hrs"
+        end
+    end
+
+    if round(value) >= 100
+        str = string(@sprintf("%.0f", value), units)
+    elseif round(value * 10) >= 100
+        str = string(@sprintf("%.1f", value), units)
+    else
+        str = string(@sprintf("%.2f", value), units)
+    end
+    return lpad(str, 6, " ")
+end
