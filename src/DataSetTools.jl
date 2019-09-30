558151b032d5a75de2607e71ea317bfa8891c57b
Takis Margaris <takismarg@gmail.com>
2019-09-30 10:51:43 +0200

    cleaning

--- src/DataSetTools.jl
+++ src/DataSetTools.jl
@@ -1,42 +1,3 @@
-#=
-function summarise(table,var::Symbol,by::Symbol)
-  y = groupby((Mean =  mean,
-              STD = std,
-              Q25 = z->quantile(z,0.25),
-              Median = z->quantile(z,0.5),
-              Q75 = z->quantile(z,0.75)),
-              table, by, select=var)
-end
-function summarise(table,var::Symbol,by::Symbol,weight::Symbol)
-  y = groupby((Mean = z-> mean(getfield(columns(z),var),weights(getfield(columns(z),weight))),
-              STD = z->std(getfield(columns(z),var),weights(getfield(columns(z),weight))),
-              Q25 = z->quantile(getfield(columns(z),var),weights(getfield(columns(z),weight)),0.25),
-              Median = z->quantile(getfield(columns(z),var),weights(getfield(columns(z),weight)),0.5),
-              Q75 = z->quantile(getfield(columns(z),var),weights(getfield(columns(z),weight)),0.75)),
-              table, by, select=(var,weight))
-end
-
-function summarise(table,var::Symbol,by::Tuple)
-  y = groupby((Mean = mean,
-              STD = std,
-              Q25 = z->quantile(z,0.25),
-              Median = z->quantile(z,0.5),
-              Q75 = z->quantile(z,0.75)),
-              table, by, select=var)
-end
-function summarise(table,var::Symbol,by::Tuple,weight::Symbol)
-  y = groupby((Mean = z-> mean(getfield(columns(z),var),weights(getfield(columns(z),weight))),
-              STD = z->std(getfield(columns(z),var),weights(getfield(columns(z),weight))),
-              Q25 = z->quantile(getfield(columns(z),var),weights(getfield(columns(z),weight)),0.25),
-              Median = z->quantile(getfield(columns(z),var),weights(getfield(columns(z),weight)),0.5),
-              Q75 = z->quantile(getfield(columns(z),var),weights(getfield(columns(z),weight)),0.75)),
-              table, by, select=(var,weight))
-end
-=#
-
-
-
-
 function reshape_results!(mc,D)
   N = D.N
   J = D.J
@@ -51,7 +12,7 @@ end
 
 function print_struct(S)
   for i in fieldnames(typeof(S))
-   println(i ," = ", getfield(S,i))
+    @info "$i = $(getfield(S,i))"
   end
 end
 
